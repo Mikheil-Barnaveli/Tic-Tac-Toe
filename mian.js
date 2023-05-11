f724861186ba9f9d64588b1e9e95ff021368a8a7
Mikheil-Barnaveli <barnavelimishiko@gmail.com>
2023-05-11 14:14:33 +0400

    adjustments
    adjustments

--- mian.js
+++ mian.js
@@ -1,119 +1,34 @@
 const body = document.querySelector('body');
 const gameBoard = document.getElementById('game-board');
-const boxes = document.querySelectorAll('.box');
-// const box1 = document.getElementById('box1');
-// const box2 = document.getElementById('box2');
-// const box3 = document.getElementById('box3');
-// const box4 = document.getElementById('box4');
-// const box5 = document.getElementById('box5');
-// const box6 = document.getElementById('box6');
-// const box7 = document.getElementById('box7');
-// const box8 = document.getElementById('box8');
-// const box9 = document.getElementById('box9');
+const box = document.getElementsByClassName('box');
+const box1 = document.getElementById('box1');
+const box2 = document.getElementById('box2');
+const box3 = document.getElementById('box3');
+const box4 = document.getElementById('box4');
+const box5 = document.getElementById('box5');
+const box6 = document.getElementById('box6');
+const box7 = document.getElementById('box7');
+const box8 = document.getElementById('box8');
+const box9 = document.getElementById('box9');
 
 
-let counter = 0;
-let gamingBoard = [boxes, boxes, boxes, boxes, boxes, boxes, boxes, boxes, boxes];
 
-gamingBoard.forEach(function(boxes) {
-    boxes.addeventListener('click', ()=>{
-        boxes.textcontent = 'X'
-    })
+let gamingBoard = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
+let playerX = true;
+
+gamingBoard.forEach(function(box) {
+        for( let i = 0; i < gamingBoard.length; i++){
+            box.addEventListener('click', ()=>{
+            if(playerX == true){
+                box.textContent = 'X'
+                playerX = false
+            }else{
+                box.textContent = 'O'
+                playerX = true
+            }
+            })
+            // if(gamingBoard[0] = gamingBoard[1] = gamingBoard[2]){
+            //     console.log("x wins");
+            // }
+        }
 })
-
-
-
-
-
-
-
-// box1.addEventListener('click', ()=>{
-//     if(counter == 0){
-//         box1.textContent = 'X'
-//         counter++;
-//     }
-//     if(counter == 1){
-//         box1.textContent = 'O'
-//         counter--;
-//     }
-// })
-// box2.addEventListener('click', ()=>{
-//     if(counter == 0){
-//         box2.textContent = 'X'
-//         counter++;
-//     }
-//     if(counter == 1){
-//         box2.textContent = 'O'
-//         counter--;
-//     }
-// })
-// box3.addEventListener('click', ()=>{
-//     if(counter == 0){
-//         box3.textContent = 'X'
-//         counter++;
-//     }
-//     if(counter == 1){
-//         box3.textContent = 'O'
-//         counter--;
-//     }
-// })
-// box4.addEventListener('click', ()=>{
-//     if(counter == 0){
-//         box4.textContent = 'X'
-//         counter++;
-//     }
-//     if(counter == 1){
-//         box4.textContent = 'O'
-//         counter--;
-//     }
-// })
-// box5.addEventListener('click', ()=>{
-//     if(counter == 0){
-//         box5.textContent = 'X'
-//         counter++;
-//     }
-//     if(counter == 1){
-//         box5.textContent = 'O'
-//         counter--;
-//     }
-// })
-// box6.addEventListener('click', ()=>{
-//     if(counter == 0){
-//         box6.textContent = 'X'
-//         counter++;
-//     }
-//     if(counter == 1){
-//         box6.textContent = 'O'
-//         counter--;
-//     }
-// })
-// box7.addEventListener('click', ()=>{
-//     if(counter == 0){
-//         box7.textContent = 'X'
-//         counter++;
-//     }
-//     if(counter == 1){
-//         box7.textContent = 'O'
-//         counter--;
-//     }
-// })
-// box8.addEventListener('click', ()=>{
-//     if(counter == 0){
-//         box8.textContent = 'X'
-//         counter++;
-//     }
-//     if(counter == 1){
-//         box8.textContent = 'O'
-//         counter--;
-//     }
-// })
-// box9.addEventListener('click', ()=>{
-//     if(counter == 0){
-//         box9.textContent = 'X'
-//         counter++;
-//     }
-//     if(counter == 1){
-//         box9.textContent = 'O'
-//         counter--;
-//     }
-// })
