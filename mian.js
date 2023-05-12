63b7c3f7b4b0de15ed35cf968a51104f1babdc8f
Mikheil-Barnaveli <barnavelimishiko@gmail.com>
2023-05-12 11:11:30 +0400

    added

--- mian.js
+++ mian.js
@@ -1,34 +1,85 @@
 const body = document.querySelector('body');
-const gameBoard = document.getElementById('game-board');
-const box = document.getElementsByClassName('box');
-const box1 = document.getElementById('box1');
-const box2 = document.getElementById('box2');
-const box3 = document.getElementById('box3');
-const box4 = document.getElementById('box4');
-const box5 = document.getElementById('box5');
-const box6 = document.getElementById('box6');
-const box7 = document.getElementById('box7');
-const box8 = document.getElementById('box8');
-const box9 = document.getElementById('box9');
+const gamebox = document.getElementById('game-box');
+const box = document.querySelectorAll('.box')
 
+console.dir(box);
 
-
-let gamingBoard = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
 let playerX = true;
 
-gamingBoard.forEach(function(box) {
-        for( let i = 0; i < gamingBoard.length; i++){
+box.forEach(function(box) {
+        
             box.addEventListener('click', ()=>{
             if(playerX == true){
                 box.textContent = 'X'
-                playerX = false
+                playerX = false;
+                box.style.pointerEvents = "none";
             }else{
                 box.textContent = 'O'
-                playerX = true
+                playerX = true;
+                box.style.pointerEvents = "none";
             }
-            })
-            // if(gamingBoard[0] = gamingBoard[1] = gamingBoard[2]){
+
+            for( let i = 0; i < box.length; i++){
+
+                if (box[0].textContent === box[1].textContent && box[1].textContent === box[2].textContent && box[0].textContent !== '') {
+                            console.log("winner");
+                            return box[0];
+                }
+
+
+
+            // function checkResult(box) {
+            //     // Check rows for a win
+            //     for (let row = 0; row < 3; row++) {
+            //       if (box[row][0] === box[row][1] && box[row][1] === box[row][2] && box[row][0] !== '') {
+            //         console.log("winner");
+            //         return box[row][0];
+            //       }
+            //     }
+                
+            //     // Check columns for a win
+            //     for (let col = 0; col < 3; col++) {
+            //       if (box[0][col] === box[1][col] && box[1][col] === box[2][col] && box[0][col] !== '') {
+            //         return box[0][col];
+            //       }
+            //     }
+                
+            //     // Check diagonals for a win
+            //     if (box[0][0] === box[1][1] && box[1][1] === box[2][2] && box[0][0] !== '') {
+            //       return box[0][0];
+            //     }
+            //     if (box[0][2] === box[1][1] && box[1][1] === box[2][0] && box[0][2] !== '') {
+            //       return box[0][2];
+            //     }
+                
+            //     // Check for a tie
+            //     let isTie = true;
+            //     for (let row = 0; row < 3; row++) {
+            //       for (let col = 0; col < 3; col++) {
+            //         if (box[row][col] === '') {
+            //           isTie = false;
+            //           break;
+            //         }
+            //       }
+            //       if (!isTie) {
+            //         break;
+            //       }
+            //     }
+            //     if (isTie) {
+            //       return 'Tie';
+            //     }
+                
+            //     // No winner yet
+            //     return null;
+            //   }
+              
+            
+
+            
+              
+            // if(box[0] = box[1] = box[2]){
             //     console.log("x wins");
             // }
         }
+})
 })
