88f645311bb6b51e1646bd35f9cc3b117d28a9bb
Mikheil-Barnaveli <barnavelimishiko@gmail.com>
2023-05-12 13:47:48 +0400

    change

--- mian.js
+++ mian.js
@@ -3,8 +3,15 @@ const gamebox = document.getElementById('game-box');
 const box = document.querySelectorAll('.box')
 const restart = document.getElementById('reset');
 const result = document.getElementById('result');
+const start = document.getElementById('start');
+const theGame = document.getElementById('the-game');
 
 
+
+start.addEventListener('click', () => {
+  start.style.display = 'none'
+  theGame.style.display = 'flex'
+})
 let playerX = true;
 console.dir(box);
 
@@ -23,14 +30,24 @@ function checkGameState() {
   ]; // all winning combinations
 
   for (let i = 0; i < winConditions.length; i++) {
+    // console.log(i, "i garet");
     let [a, b, c] = winConditions[i];
-    if (boxValues[a] && boxValues[a] === boxValues[b] && boxValues[a] === boxValues[c]) {
-      console.log(`Winner: ${boxValues[a]}`);
-      result.textContent = `Winner: ${boxValues[a]}`;
-      // check for a winner
-      return boxValues[a];
+    if (boxValues[a] == "X" || boxValues[a] == "O") {
+      if (boxValues[a] === boxValues[b] && boxValues[a] === boxValues[c]) {
+        // console.log(i, "i shignit");
+        // console.log(boxValues);
+        // console.log(a, "a")
+        // console.log(boxValues[a])
+        console.log(`Winner: ${boxValues[a]}`);
+        result.textContent = `Winner: ${boxValues[a]}`;
+        // check for a winner
+        return boxValues[a];
+      }
+
     }
+    
   }
+  console.log(result, "result1");
   if (boxValues.every((cell) => cell !== "")) {
     // check for a tie
     result.textContent = "Tie"
@@ -58,13 +75,13 @@ function restartGame() {
   box.forEach((box) => {
     box.textContent = '';
   });
-  
+
   currentPlayer = 'X';
-  
+
   box.forEach((box) => {
     box.style.pointerEvents = 'auto';
   });
-  
+
   // // Remove winner class from gamebox
   // gamebox.classList.remove('winner');
 }
@@ -74,7 +91,7 @@ box.forEach(box => {
   box.addEventListener('click', boxClick);
 });
 
-restart.addEventListener('click', ()=>{
+restart.addEventListener('click', () => {
   for (let i = 0; i < box.length; i++) {
     result.textContent = '';
     playerX = true;
@@ -83,22 +100,22 @@ restart.addEventListener('click', ()=>{
 })
 
 
-box.forEach(function(box) {
-  
-        
-    box.addEventListener('click', ()=>{
-    if(playerX == true){
-        box.textContent = 'X'
-        playerX = false;
-        box.style.pointerEvents = "none";
-        checkGameState()
-        checkGameOver()
-    }else{
-        box.textContent = 'O'
-        playerX = true;
-        box.style.pointerEvents = "none";
-        checkGameState()
-        checkGameOver()
+box.forEach(function (box) {
+
+
+  box.addEventListener('click', () => {
+    if (playerX == true) {
+      box.textContent = 'X'
+      playerX = false;
+      box.style.pointerEvents = "none";
+      checkGameState()
+      checkGameOver()
+    } else {
+      box.textContent = 'O'
+      playerX = true;
+      box.style.pointerEvents = "none";
+      checkGameState()
+      checkGameOver()
     }
-})
+  })
 })
